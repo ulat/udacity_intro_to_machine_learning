51678ab68fbffceee4799bd806be08d45b5a7cbc
ulat <vito@kaffeeschluerfer.com>
2016-10-25 14:16:06 +0200

    Finished Naive Bayes Classifier

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,7 +14,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
+import sklearn.metrics as metrics
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -26,7 +27,16 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
+gnb = GaussianNB()
+t0 = time()
+gnb.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t0 = time()
+y_pred = gnb.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+print "Number of mislabeld points out of a total %d points: %d"\
+      %(features_test.shape[0], (labels_test != y_pred).sum())
+print gnb.score(features_test, labels_test)
 
 #########################################################
 
