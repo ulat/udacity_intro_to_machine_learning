d5c7122e92e0cfac0219ee084a7dd75a7f62a46e
ulat <vito@kaffeeschluerfer.com>
2016-11-27 12:04:43 +0100

    finished chapter outliers

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -10,10 +10,35 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+
+### remove the outlier ("TOTAL" - not a datapoint but a spreadsheet summary line)
+data_dict.pop('TOTAL',0)
+
+# We would argue that there’s 4 more outliers to investigate; let's look at a
+# couple of them. Two people made bonuses of at least 5 million dollars, and
+# a salary of over 1 million dollars; in other words, they made out like bandits.
+# What are the names associated with those points?
+names_to_remove = [name for name in data_dict.keys() \
+    if data_dict[name]['bonus'] != 'NaN' \
+    and int(data_dict[name]['bonus']) >= 5000000 \
+    and data_dict[name]['salary'] != 'NaN' \
+    and int(data_dict[name]['salary']) > 1000000
+]
+print "names of persons who made more than 5Mio bonus and more than 1Mio salary:", \
+    sorted(names_to_remove)
+
+
 data = featureFormat(data_dict, features)
 
 
+
+
 ### your code below
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
-
-
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -7,14 +7,39 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
+
+    # first approach just using list comprehension
+    import timeit
+    start = timeit.default_timer()
     cleaned_data = []
+    cleaned_data = [(age, net_worth, (prediction-net_worth)**2)\
+        for age, net_worth, prediction \
+        in zip(ages, net_worths, predictions)]
+    last_index = int(len(cleaned_data)*0.9-1)
+    cleaned_data = sorted(cleaned_data, key = lambda t: t[2])
+    cleaned_data = cleaned_data[0:last_index]
+    stop = timeit.default_timer()
+    runtime1 = stop - start
 
-    ### your code goes here
-
-    
+    # second approach. converting lists to numpy arrays, doing the math and
+    # converting back to a list of tuples.
+    start = timeit.default_timer()
+    import numpy as np
+    predictions_array = np.reshape(np.array(predictions), (len(predictions), 1))
+    ages_array = np.reshape(np.array(ages), (len(ages), 1))
+    net_worths_array = np.reshape(np.array(net_worths), (len(net_worths), 1))
+    cleaned_data = np.concatenate((ages_array, net_worths_array, (predictions_array-net_worths_array)**2), axis=1)
+    cleaned_data = cleaned_data[cleaned_data[:,2].argsort()]
+    cleaned_data = cleaned_data[:last_index+1, :]
+    #print "cleaned_data size:", cleaned_data.size, "dimens: ", cleaned_data.shape
+    cleaned_data = [tuple(row) for row in cleaned_data]
+    #print "length of cleaned_data:", len(cleaned_data)
+    runtime2 = stop - start
+    #print "runtime with lists: ", runtime1
+    #print "runtime with np.arrays: ", runtime2
+    #
+    # The second approach with numpy arrays seems to work a bit faster.
     return cleaned_data
-
