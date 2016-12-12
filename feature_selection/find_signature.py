ffeaa715544c98461c353ec2d5864b97f1fed463
ulat <vito@kaffeeschluerfer.com>
2016-12-12 20:30:02 +0100

    finished feature selection

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -19,12 +19,11 @@ authors = pickle.load( open(authors_file, "r") )
 ### remainder go into training)
 ### feature matrices changed to dense representations for compatibility with
 ### classifier functions in versions 0.15.2 and earlier
-from sklearn import cross_validation
-features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+from sklearn import model_selection
+features_train, features_test, labels_train, labels_test = model_selection.train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
-                             stop_words='english')
+vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, stop_words='english')
 features_train = vectorizer.fit_transform(features_train)
 features_test  = vectorizer.transform(features_test).toarray()
 
@@ -38,6 +37,27 @@ labels_train   = labels_train[:150]
 
 
 ### your code goes here
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
 
+pred = clf.predict(features_train)
+acc = accuracy_score(labels_train, pred)*1.0
+print "Accuracy on Trainingset: (Should be 1) ", acc
+
+pred = clf.predict(features_test)
+acc = accuracy_score(labels_test, pred)*1.0
+
+print "Accuracy on Test-Set: ", acc
+
+## Print most important features
+feature_importance_threshold = 0.2
+
+
+important_features = [(feature, importance) for feature, importance in enumerate(clf.feature_importances_) if importance > feature_importance_threshold]
+
+
+print "highest rating feature: ", important_features
--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -42,32 +42,33 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
         temp_counter += 1
-        if temp_counter < 200 or True:  # 'or True: flag to process all emails!
-            path = os.path.join('..', path[:-1])
-            print path
-            email = open(path, "r")
+        #if temp_counter < 200 or True:  # 'or True: flag to process all emails!
+        path = os.path.join('..', path[:-1])
+        print path
+        email = open(path, "r")
 
-            ### use parseOutText to extract the text from the opened email
-            mail_text = parseOutText(email)
+        ### use parseOutText to extract the text from the opened email
+        mail_text = parseOutText(email)
 
-            ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
+        ### use str.replace() to remove any instances of the words
+        ### ["sara", "shackleton", "chris", "germani"]
 
-            stop_words = ["sara", "shackleton", "chris", "germani"]
+        #stop_words = ["sara", "shackleton", "chris", "germani"]
+        stop_words = ["sara", "shackleton", "chris", "germani", "sshacklensf", "cgermannsf"]
 
-            for w in stop_words:
-                mail_text = mail_text.replace(w, "")
+        for w in stop_words:
+            mail_text = mail_text.replace(w, "")
 
-            ### append the text to word_data
-            word_data.append(mail_text)
+        ### append the text to word_data
+        word_data.append(mail_text)
 
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-            if from_person == "sara":
-                from_data.append(0)
-            else:
-                from_data.append(1)
+        ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+        if name == "sara":
+            from_data.append(0)
+        else:
+            from_data.append(1)
 
-            email.close()
+        email.close()
 
 print "emails processed"
 from_sara.close()
@@ -83,12 +84,16 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 ### in Part 4, do TfIdf vectorization here
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-vectorizer = CountVectorizer(stop_words='english')
+vectorizer = TfidfVectorizer(stop_words='english')
 vectorizer.fit_transform(word_data)
 
-pickle.dump(vectorizer, open('tfidf_vectorizer.pkl', 'w'))
+feature_names = vectorizer.get_feature_names()
+
+#print "feature_name with highest rate: ", feature_names.index("sshacklensf")
+
+
 
 feature_names = vectorizer.get_feature_names()
 num_unique_words_tfidf = len(feature_names)
